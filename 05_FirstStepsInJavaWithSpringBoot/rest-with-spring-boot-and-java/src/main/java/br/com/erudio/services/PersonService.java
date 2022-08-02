113cd556964b6c218ffb8a41aa313874c657c5d8
RBRibeiro00 <romuloribeirob@hotmail.com>
2022-08-02 09:34:13 -0300

    method find all people

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/services/PersonService.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/services/PersonService.java
@@ -1,5 +1,7 @@
 package br.com.erudio.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +14,15 @@ public class PersonService {
 
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonService.class.getName());
+	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<>();	
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
 
 	public Person findById(String id) {
 
@@ -24,6 +35,17 @@ public class PersonService {
 		person.setAddress("Belo Horizonte - Minas Gerais - Brasil");
 		person.setGender("Male");
 
+		return person;
+	}
+
+	private Person mockPerson(int i) {
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Some address in Brazil " + i);
+		person.setGender("Male");
 		return person;
 	}
 
