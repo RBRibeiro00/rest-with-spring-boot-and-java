bceab040754b4dacb1587e5af9c55b0df766d939
RBRibeiro00 <romuloribeirob@hotmail.com>
2022-08-02 09:50:02 -0300

    methods post, put and delete for person

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class PersonController {
 
 	@Autowired
 	private PersonService personService;
-	
+
 	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Person> findAllPeople() {
 		return personService.findAll();
@@ -27,6 +28,21 @@ public class PersonController {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable(value = "id") String id) throws Exception {
 		return personService.findById(id);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) throws Exception {
+		return personService.create(person);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) throws Exception {
+		return personService.update(person);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable(value = "id") String id) throws Exception {
+		personService.delete(id);
 	}
 
 }
