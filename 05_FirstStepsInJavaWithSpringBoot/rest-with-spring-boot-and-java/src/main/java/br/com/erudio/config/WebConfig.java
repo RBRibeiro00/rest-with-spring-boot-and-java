aa2136732c77bb0fad3c6a8075fbbbb9c4700c84
RBRibeiro00 <romuloribeirob@hotmail.com>
2022-08-15 19:25:50 -0300

    Configurando Cross-Origin Resource Sharing (CORS)

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/config/WebConfig.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/config/WebConfig.java
@@ -2,27 +2,41 @@ package br.com.erudio.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.com.erudio.serialization.converter.YamlJackson2HttpMessageConverter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	
+
 	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+	@Value("${cors.originPatterns:default}")
+	private String corsOriginPatterns = "";
 
 	@Override
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new YamlJackson2HttpMessageConverter());
-		//WebMvcConfigurer.super.extendMessageConverters(converters);
 	}
 
 	@Override
-	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) { 
+	public void addCorsMappings(CorsRegistry registry) {
+		var allowedOrigins = corsOriginPatterns.split(",");
+		registry.addMapping("/**")
+//			.allowedMethods("GET", "POST", "PUT")
+			.allowedMethods("*")
+			.allowedOrigins(allowedOrigins)
+			.allowCredentials(true);
+	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 //		WebMvcConfigurer.super.configureContentNegotiation(configurer.favorParameter(true) // VIA QUERY PARAM
 //				.parameterName("mediaType")
 //				.ignoreAcceptHeader(true)
@@ -31,13 +45,11 @@ public class WebConfig implements WebMvcConfigurer {
 //				.mediaType("json", MediaType.APPLICATION_JSON)
 //				.mediaType("xml", MediaType.APPLICATION_XML)
 //				.mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML));
-		
-		WebMvcConfigurer.super.configureContentNegotiation(configurer.favorParameter(false) // VIA HEADER PARAM (POSTMAN)
-				.ignoreAcceptHeader(false)
-				.useRegisteredExtensionsOnly(false)
-				.defaultContentType(MediaType.APPLICATION_JSON)
-				.mediaType("json", MediaType.APPLICATION_JSON)
-				.mediaType("xml", MediaType.APPLICATION_XML)
-				.mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML));
+
+		WebMvcConfigurer.super.configureContentNegotiation(configurer.favorParameter(false) // VIA HEADER PARAM
+																							// (POSTMAN)
+				.ignoreAcceptHeader(false).useRegisteredExtensionsOnly(false)
+				.defaultContentType(MediaType.APPLICATION_JSON).mediaType("json", MediaType.APPLICATION_JSON)
+				.mediaType("xml", MediaType.APPLICATION_XML).mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML));
 	}
 }
