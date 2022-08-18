a46db27a29e39e8128bccbed0c04101699a96154
RBRibeiro00 <romuloribeirob@hotmail.com>
2022-08-18 18:00:20 -0300

    Including spring security and jwt dependencies / Includiin flyway migrations for user permission

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/model/User.java
@@ -0,0 +1,85 @@
+package br.com.erudio.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails, Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "user_name", unique = true)
+	private String userName;
+	
+	@Column(name = "full_name")
+	private String fullName;
+	
+	@Column
+	private String password;
+	
+	@Column(name = "account_non_expired")
+	private Boolean accountNonExpired;
+	
+	@Column(name = "account_non_locked")
+	private Boolean accountNonLocked;
+	
+	@Column(name = "credentials_non_expired")
+	private Boolean credentialsNonExpired;
+	
+	@Column
+	private Boolean enabled;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "user_permission", joinColumns = {@JoinColumn (name = "id_user")}, 
+		inverseJoinColumns = {@JoinColumn (name = "id_permission")})
+	private List<Permission> permissions;
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+	@Override
+	public String getPassword() {
+		return null;
+	}
+	@Override
+	public String getUsername() {
+		return null;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+}
